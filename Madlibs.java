f5d0373e66add672da17a9f33193377233bf1286
Kaz <Kaz@fake.com>
2024-09-27 11:12:58 -0700

    find nouns, adj, verbs etc in the madlib

--- Madlibs.java
+++ Madlibs.java
@@ -3,14 +3,23 @@ public class Madlibs
 {
     public static void main(String[] args) 
     {
+        //Creates active madlib and stores the extra 2
+        String activeMadlib = "I went to the animal <noun>, the <plural_noun> and the <number> beasts were there.";
+        //activeMadlib = "Once upon a time in a <adjective> land, a <noun> <adverb> grew.";
+        //activeMadlib = "I once knew a <noun> with many pet <plural noun> that were <adjective>";
         //Creates madlib scanner
-        Scanner madLibInput = new Scanner(System.in);
-        //Ask user for first madlib
-        System.out.println("Enter a verb ");
-        String verbOne = madLibInput.nextLine();
-        System.out.println("Your madlib is: " + verbOne);
-        String activeMadLib = "I went to the animal <noun>, the <plural_noun> and the <number> beasts were there.";
-        //activeMadLib = "Once upon a time in a <adjective> land, a <noun> <adverb> grew.";
+        Scanner madlibInput = new Scanner(System.in);
+        //finds location of all filler words 
+        int firstOpen = activeMadlib.indexOf("<");
+        int firstClose = activeMadlib.indexOf(">");
+        int secondOpen = activeMadlib.indexOf("<", firstClose+ 1);
+        int secondClose = activeMadlib.indexOf(">", firstClose + 1);
+        int thirdOpen = activeMadlib.indexOf("<", secondClose + 1);
+        int thirdClose = activeMadlib.indexOf(">", secondClose + 1);
+        
+        
+
+        
 
     }
 }
